b4816b3a4fc530bf42974eb096aa43633048e035
Sergio Oliveira <sergio.oliveira@coralblocks.com>
2024-12-24 11:21:52 -0300

    Addine new JNR version 2.2.17

--- bin/mac/jnr/compileJava.sh
+++ bin/mac/jnr/compileJava.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-javac -d classes -cp jars/jnr-ffi-2.2.13-all.jar:. src/main/java/com/coralblocks/javatocppandback/jnr/HelloWorld.java
+javac -d classes -cp jars/jnr-ffi-2.2.17-all.jar:. src/main/java/com/coralblocks/javatocppandback/jnr/HelloWorld.java
 
